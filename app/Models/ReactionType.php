75ab3b332264bb6d5432cebb53e7db597b49dd47
Jon Garbayo <jon.garbayo@gmail.com>
2023-08-17 13:03:14 +0200

    🗃️ Create the ReactionType model

---
+++ app/Models/ReactionType.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ReactionType extends Model
+{
+    use HasFactory;
+
+    // The Eloquent convention for the tables names is to put an "s" at the end
+    // of the model name. But in this case, with a multi-word model name, the
+    // first word must also be plural to stay grammatically correct.
+    // As this breaks the automation of Eloquent, the table name is explicitly
+    // specified here.
+    protected $table = 'reactions_types';
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+}
---
+++ database/migrations/2023_08_17_101153_create_reactions_types_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReactionsTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reactions_types', function (Blueprint $table) {
+            $table->id();
+            $table->string('tag', 5)->unique();
+            $table->string('name', 10)->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reactions_types');
+    }
+}
