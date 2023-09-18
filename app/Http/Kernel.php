0b6acce0a53f2003b5b347f3b088adf9ab7a1ef0
Jon Garbayo <jon.garbayo@gmail.com>
2023-09-18 17:29:23 +0200

    🔒️ Add a middleware to logout disabled users
    As it's not possible to logout another user than the current one when sessions are stored in files with Laravel, this middleware intercepts any further request from disabled users to log them out.
    To be more complete and user friendly (and secured in some ways), a web socket signal should be added to that procedure, to logout a user a soon as he/she is disabled, instead of waiting for his/her next request. But this first layer of security is necessary to handle the cases where the browser is closed.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -66,5 +66,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'logout_disabled_user' => \App\Http\Middleware\LogoutDisabledUser::class
     ];
 }
