b9b62375c8cff1ade69e8276476eefc4c30f4408
Jon Garbayo <jon.garbayo@gmail.com>
2023-10-12 11:50:12 +0200

    🔧 Add the expiration time of a member invitation in the app config
    Doing so respects the seperation of concerns logic of the Twelve-Factor App methodology, instead of hard coding the value in the code base.

--- config/business.php
+++ config/business.php
@@ -2,6 +2,16 @@
 
 return [
 
-    
+    /*
+    |--------------------------------------------------------------------------
+    | Member invitation TTL
+    |--------------------------------------------------------------------------
+    |
+    | Time left before a member invitation expires, in seconds. This value is
+    | used directly as the TTL setting in the Redis SET command.
+    |
+    */
+
+    'invitation_ttl' => 2592000 // 30 days
 
 ];
