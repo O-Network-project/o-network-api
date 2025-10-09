0f27dec9dd446476377a8fc82e209171bd285c80
Jon Garbayo <jon.garbayo.pro@gmail.com>
2025-10-09 16:44:00 +0200

    🌱 Improve `UserSeeder` performance by avoiding an unnecessary repeated database query
    Each organization's users were fetched repeatedly during every loop iteration, only to check whether they had an admin. With the new strategy, all admins in the database are fetched in a single query, reducing the number of database calls.

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -38,13 +38,17 @@ class UserSeeder extends Seeder
         // the creation of the admin (automatically the first user)
         $organizations->pop();
 
+        /** @var Collection<int> */
+        $organizationWithAdminIds = Organization::whereRelation('users', 'role_id', 2)
+            ->pluck('id');
+
         // Each seeded organization (except the last one, check the above
         // comment) will have 1 admin and 10 members
-        $organizations->each(function (Organization $organization) use ($volume, $users, $factory) {
-            // The below conditions allows the UserSeeder to be launched
-            // multiple times without ending with multiple admins, as only 1
-            // admin should exist in each organization
-            if ($organization->users->where('role_id', 2)->count() === 0) {
+        $organizations->each(function (Organization $organization) use ($volume, $organizationWithAdminIds, $users, $factory) {
+            // The below condition allows the UserSeeder to be launched multiple
+            // times without generating multiple admins, as only 1 admin should
+            // exist in each organization
+            if (!$organizationWithAdminIds->contains($organization->id)) {
                 $users->push($factory
                     ->admin()
                     ->for($organization)
